0cfb8f2a037dbe6efd2572e476cc297ceff75a49
Ignacio Reyna <ignacioreyna56@gmail.com>
2019-07-05 07:24:44 -0500

    [ Alula ] - Ajustes de glow con ajustes del cliente

--- assets/js/generals.js
+++ assets/js/generals.js
@@ -1,15 +1,38 @@
-/**
- * IPG Mediabrands
- *
- *
- * This file contains IPG Mediabrands Information
- * Use, disclosure or reproduction is prohibited.
- *
- * @author    IPG Mediabrands / Benjamín García
-
- */
-
-
 $( document ).ready(function() {
-	//Start the Party
+	
+	$('.especiales-bote-uno').hide();
+	$('.especiales-bote-dos').hide();
+	$('.especiales-bote-tres').hide();
+
+	$('.alula-bote-dos').hide();
+	$('.alula-bote-tres').hide();
+
+	$('.nubes-comfort-dos').hide();
+	$('.nubes-comfort-tres').hide();
+
+
+	
+
+	setTimeout(function () {
+		$('.especiales-bote-dos').show();
+		$('.alula-bote-dos').show();
+		$('.nubes-comfort-dos').show();
+		$('.especiales-bote-uno').attr('src', 'media/images/bote-uno.png');
+		//$('.lata-uno').addClass('glow');
+	}, 800);
+	setTimeout(function () {
+		$('.especiales-bote-tres').show();
+		$('.alula-bote-tres').show();
+		$('.nubes-comfort-tres').show();
+		$('.especiales-bote-dos').attr('src', 'media/images/bote-dos.png');
+		//$('.lata-uno').addClass('glow');
+	}, 1600);
+	setTimeout(function () {
+		$('.especiales-bote-tres').attr('src', 'media/images/bote-tres.png');
+		//$('.lata-uno').addClass('glow');
+	}, 2400);
+
+
+
+
 });
