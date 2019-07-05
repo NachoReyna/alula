dbcaabd1a7ac63d7833197c35bd218aae82721c1
Ignacio Reyna <ignacioreyna56@gmail.com>
2019-07-05 13:37:01 -0500

    [ Alula ] - botes

--- assets/js/generals.js
+++ assets/js/generals.js
@@ -14,7 +14,7 @@ $( document ).ready(function() {
 		$('.especiales-bote-dos').show();
 		$('.alula-bote-dos').show();
 		$('.nubes-comfort-dos').show();
-		$('.especiales-bote-uno').attr('src', 'media/images/bote-uno.png');
+		$('.especiales-bote-uno').attr('src', 'media/images/bote-uno_.png');
 		$('.alula-bote-uno').attr('src', 'media/images/bebe-tres-n-.png');
 		//$('.lata-uno').addClass('glow');
 	}, 800);
@@ -22,13 +22,13 @@ $( document ).ready(function() {
 		$('.especiales-bote-tres').show();
 		$('.alula-bote-tres').show();
 		$('.nubes-comfort-tres').show();
-		$('.especiales-bote-dos').attr('src', 'media/images/bote-dos.png');
+		$('.especiales-bote-dos').attr('src', 'media/images/bote-dos_.png');
 		$('.alula-bote-dos').attr('src', 'media/images/bebe-dos-nuevo-.png');
 
 		//$('.lata-uno').addClass('glow');
 	}, 1600);
 	setTimeout(function () {
-		$('.especiales-bote-tres').attr('src', 'media/images/bote-tres.png');
+		$('.especiales-bote-tres').attr('src', 'media/images/bote-tres_.png');
 		$('.alula-bote-tres').attr('src', 'media/images/bebe-tres-nuevo-.png');
 
 		//$('.lata-uno').addClass('glow');
